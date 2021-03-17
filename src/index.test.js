2aea57825b69fb5cfce9ba9b47ed4fb443abf54a
Robson Nascimento <robsonluisn@icloud.com>
2021-03-17 20:44:10 +0000

    update error test CI

--- src/index.test.js
+++ src/index.test.js
@@ -3,18 +3,18 @@ import jsdom from 'jsdom'
 import fs from 'fs'
 
 describe('Our first test', () => {
-  it('Should pass', () => {
+  it('should pass', () => {
     expect(true).to.equal(true)
   })
 })
 
 describe('index.html', () => {
-  it('should say hello', () => {
+  it('should have h1 that says users', () => {
     const index = fs.readFileSync('./src/index.html', 'utf-8')
     const { JSDOM } = jsdom
     const dom = new JSDOM(index)
-    const h1 = dom.window.document.getElementsByTagName('h1')()
-    expect(h1.innerHtml).to.equal('Hello World!')
+    const h1 = dom.window.document.getElementsByTagName('h1')[0]
+    expect(h1.innerHTML).to.equal('Users')
     dom.window.close()
   })
 })
