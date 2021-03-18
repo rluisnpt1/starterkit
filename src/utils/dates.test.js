6df53061aec2f68a889b641c126cbdfde5754686
Robson Nascimento <robsonluisn@icloud.com>
2021-03-18 19:27:22 +0000

    add mockAPi mock data and utils

---
+++ src/utils/dates.test.js
@@ -0,0 +1,24 @@
+import { getFormattedDateTime } from './dates'
+import { expect } from 'chai'
+
+describe('Date Helper', () => {
+  describe('getFormattedDateTime', () => {
+    it('returns mm/dd hh:mm:ss formatted time when passed a date', () => {
+      // arrange
+      // The 7 numbers specify the year, month, day, hour, minute, second, and millisecond, in that order
+      const date = new Date(99, 0, 24, 11, 33, 30, 0)
+
+      // assert
+      expect(getFormattedDateTime(date)).to.equals('1/24 11:33:30')
+    })
+
+    it('pads single digit minute and second values with leading zeros', () => {
+      // arrange
+      // The 7 numbers specify the year, month, day, hour, minute, second, and millisecond, in that order
+      const date = new Date(99, 0, 4, 11, 3, 2, 0)
+
+      // assert
+      expect(getFormattedDateTime(date)).to.equal('1/4 11:03:02')
+    })
+  })
+})
