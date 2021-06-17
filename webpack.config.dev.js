1367a7a597174ca546fba5f4de746064425de581
Robson Nascimento <robsonluisn@icloud.com>
2021-06-17 17:28:16 +0100

    update outdated packages

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,25 +1,26 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 export default {
-  mode: "development",
-  devtool: "eval-source-map",
-  entry: "./src/index.js",
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "src"),
-    publicPath: "/",
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   plugins: [
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
+      favicon: 'src/favicon.ico',
     }),
   ],
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
   },
-};
+}
